3744867f84093015a540ad919d1cfd62c97a8c99
Tay Starr <tstarrroberts@gmail.com>
2022-03-31 10:02:35 -0400

    add form handler

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,27 @@
+var cityFormEl = document.querySelector("#city-form");
 var cityInputEl = document.querySelector("#city");
+var searchButton = document.querySelector("#search-button");
 
 var apiKey = "277f0fb7aae3b4c0ac697e387ae15bf4";
 
-var cityWeather = function () {
-    var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&units=imperial&APPID=" + apiKey;
+var cityFormHandler = function(event) {
+
+    event.preventDefault();
+
+    var city = cityInputEl.value.trim();
+
+    if (city) {
+        getCityWeather(city);
+        cityInputEl.value = "";
+    } else {
+        alert("Please try again");
+    }
+
+    console.log(event);
+};
+
+var getCityWeather = function (city) {
+    var cityUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&APPID=" + apiKey;
     
     fetch(cityUrl).then(function(response) {
         response.json().then(function(data) {
@@ -12,7 +30,7 @@ var cityWeather = function () {
     });
 };
 
-var getWeather = function() {
+var getUvI = function() {
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=" + apiKey;
 
     fetch(apiUrl).then(function(response) {
@@ -22,5 +40,7 @@ var getWeather = function() {
     });
 };
 
-getWeather();
-cityWeather();
+getUvI();
+getCityWeather('philadelphia');
+
+cityFormEl.addEventListener("submit", cityFormHandler);
